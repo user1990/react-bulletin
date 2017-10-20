3dc221a0a7d5458859d1271563d88dfb84a0b971
user1990 <paulius.rimg1990@gmail.com>
2017-10-20 09:37:51 +0300

    Refactoring Layout View

---
+++ src/components/Layout/index.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import { withStyles, MuiThemeProvider } from 'material-ui/styles'
+import classNames from 'classnames'
+import theme from './fusTheme'
+import TopBar from './appBar'
+import SideBar from './drawer'
+import styles from './styles'
+
+class Layout extends Component {
+  constructor () {
+    super()
+    this.state = {
+      open: true,
+      anchorEl: null,
+      openMenu: false
+    }
+    this.handleDrawerOpen = this.handleDrawerOpen.bind(this)
+    this.handleDrawerClose = this.handleDrawerClose.bind(this)
+  }
+  handleDrawerOpen () {
+    this.setState({ open: true })
+  }
+  handleDrawerClose () {
+    this.setState({ open: false })
+  }
+  handleClick = e => {
+    this.setState({ openMenu: true, anchorEl: e.currentTarget })
+  }
+  handleRequestClose = () => {
+    this.setState({ openMenu: false })
+  }
+
+  render () {
+    const classes = this.props.classes
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div className={classes.root}>
+          <TopBar
+            open={this.state.open}
+            anchorEl={this.state.anchorEl}
+            classes={this.props.classes}
+            openMenu={this.state.openMenu}
+            handleDrawerOpen={this.handleDrawerOpen}
+            handleClick={this.handleClick}
+            handleRequestClose={this.handleRequestClose}
+          />
+          <SideBar
+            open={this.state.open}
+            classes={this.props.classes}
+            handleDrawerClose={this.handleDrawerClose}
+          />
+          <div className={classes.appFrame}>
+            <main
+              className={classNames(
+                classes.content,
+                this.state.open && classes.contentShift
+              )}
+            >
+              {this.props.children}
+            </main>
+          </div>
+        </div>
+      </MuiThemeProvider>
+    )
+  }
+}
+
+export default withStyles(styles)(Layout)
