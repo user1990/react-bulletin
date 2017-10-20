09da6522027de6c32378a7b7cb138ad5c889f57a
user1990 <paulius.rimg1990@gmail.com>
2017-10-20 09:53:00 +0300

    Add linting

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ['standard', 'plugin:react/recommended'],
+  plugins: ['standard', 'react', 'promise'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    'react/prop-types': 0,
+    quotes: '["error", "single", { "allowTemplateLiterals": true }]'
+  }
+}
