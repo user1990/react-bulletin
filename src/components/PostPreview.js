05d91b1c34e8e29a15ca31307708a9de47c4d4ee
user1990 <paulius.rimg1990@gmail.com>
2017-10-25 13:15:05 +0300

    Added continue reading in grid view

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 import Divider from 'material-ui/Divider'
 import { withStyles } from 'material-ui/styles'
 import { blue, grey } from 'material-ui/colors'
-import Attachements from './Attachments'
+import Attachments from './Attachments'
 
 const styles = {
   card: {
@@ -27,6 +27,11 @@ const styles = {
   link: {
     color: 'inherit',
     textDecoration: 'none'
+  },
+  continue: {
+    textDecoration: 'none',
+    color: blue[800],
+    fontSize: '16px'
   }
 }
 
@@ -40,58 +45,79 @@ const CardImage = ({ mediaStyle, imageURL, id }) => {
   )
 }
 
-const PostPreview = ({
-  classes,
-  title,
-  imageURL,
-  date,
-  id,
-  style,
-  category,
-  content
-}) => {
-  const postDate = new Date(date).toLocaleDateString()
-  return (
-    <Card className={classes.card} style={style}>
-      <CardImage mediaStyle={classes.media} imageURL={imageURL} id={id} />
-      <CardContent>
-        <Typography type="caption" className={classes.categoryColor}>
-          {category.toUpperCase()}
-        </Typography>
-        <Typography
-          type="headline"
-          className={classes.titleColor}
-          component="h2"
-        >
-          <Link className={classes.link} to={`/post/${id}`}>
-            {title}
-          </Link>
-        </Typography>
-        <Typography
-          className={classes.dateColor}
-          type="subheading"
-          component="h3"
-        >
-          {postDate}
-        </Typography>
-        <Typography
-          type="body2"
-          gutterBottom
-          dangerouslySetInnerHTML={{ __html: content }}
-        />
-        {/* TODO: - Add logic to test if there are attachments */}
-        <Attachements
-        // pass the attachments as props here
-        />
-      </CardContent>
-      <Divider />
-      <CardActions>
-        <Button href={`/post/${id}`} dense color="primary">
-          Read More
-        </Button>
-      </CardActions>
-    </Card>
-  )
+class PostPreview extends React.Component {
+  state = {
+    cRead: false,
+    content: ''
+  }
+  sanitizeContent = content => {
+    const regex = /(<([^>])>)/gi
+    let newContent = content.replace(regex, '')
+    if (!this.props.trim) return newContent
+    if (newContent.split(' ').length > 55) {
+      newContent = newContent
+        .split(' ')
+        .slice(0, 55)
+        .join(' ')
+      this.setState({
+        cRead: true
+      })
+    }
+    return newContent
+  }
+  componentWillMount () {
+    this.setState({
+      content: this.sanitizeContent(this.props.content)
+    })
+  }
+  render () {
+    const { classes, title, imageURL, date, id, style, category } = this.props
+    const postDate = new Date(date).toLocaleDateString()
+    return (
+      <Card className={classes.card} style={style}>
+        <CardImage mediaStyle={classes.media} imageURL={imageURL} id={id} />
+        <CardContent>
+          <Typography type="caption" className={classes.categoryColor}>
+            {category.toUpperCase()}
+          </Typography>
+          <Typography
+            type="headline"
+            className={classes.titleColor}
+            component="h2"
+          >
+            <Link className={classes.link} to={`/post/${id}`}>
+              {title}
+            </Link>
+          </Typography>
+          <Typography
+            className={classes.dateColor}
+            type="subheading"
+            component="h3"
+          >
+            {postDate}
+          </Typography>
+          <Typography type='body2' gutterBottom>
+            {this.state.content}
+            {this.state.cRead && (
+              <Link className={classes.continue} to={`/post/${id}`}>
+                {' '}
+                ...Continue Reading
+              </Link>
+            )}
+          </Typography>
+          <Attachments
+          // pass the attachments as props here
+          />
+        </CardContent>
+        <Divider />
+        <CardActions>
+          <Button href={`/post/${id}`} dense color="primary">
+            Read More
+          </Button>
+        </CardActions>
+      </Card>
+    )
+  }
 }
 
 export default withStyles(styles)(PostPreview)
